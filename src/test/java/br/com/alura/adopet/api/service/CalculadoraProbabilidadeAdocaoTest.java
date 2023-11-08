4e501fe178ecb99c9bfb2363d50d506162cdd19a
flavio.santana <9090flavio@gmail.com>
2023-11-08 18:33:04 -0300

    feat: O padrão Triple A (ARRANGE, ACT, ASSERT)

--- src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
+++ src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -18,44 +18,18 @@ class CalculadoraProbabilidadeAdocaoTest {
     @DisplayName("Testa Probabilidade de Adoção de Gato novo com pouco peso")
     void deveriaCalcularProbabilidadeAdocaoGatoAlta() {
 
-
-        Pet pet = arrangePet(4, 4.0f);
+        Pet pet = arrangePet(TipoPet.GATO,4, 4.0f);
 
         ProbabilidadeAdocao probabilidade = actCalculateProbability(pet);
 
         assertEquals(ProbabilidadeAdocao.ALTA, probabilidade);
     }
 
-    private static ProbabilidadeAdocao actCalculateProbability(Pet pet) {
-        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
-        ProbabilidadeAdocao probabilidade = calculadora.calcular(pet);
-        return probabilidade;
-    }
-
-    private static Pet arrangePet(int idade, float peso) {
-        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
-                "Abrigo feliz",
-                "94999999999",
-                "abrigofeliz@email.com.br"
-        ));
-
-        Pet pet = new Pet(new CadastroPetDto(
-                TipoPet.GATO,
-                "Miau",
-                "Siames",
-                idade,
-                "Cinza",
-                peso
-        ), abrigo);
-        return pet;
-    }
-
     @Test
     @DisplayName("Testa Probabilidade de Adoção de Gato velho com pouco peso")
     void deveriaCalcularProbabilidadeAdocaoGatoMedia() {
 
-
-        Pet pet = arrangePet(15, 4.0f);
+        Pet pet = arrangePet(TipoPet.GATO, 15, 4.0f);
 
         ProbabilidadeAdocao probabilidade = actCalculateProbability(pet);
 
@@ -66,11 +40,46 @@ class CalculadoraProbabilidadeAdocaoTest {
     @DisplayName("Testa Probabilidade de Adoção de Gato velho com muito peso")
     void deveriaCalcularProbabilidadeAdocaoGatoBaixa() {
 
-
-        Pet pet = arrangePet(14, 11.0f);
+        Pet pet = arrangePet(TipoPet.GATO, 14, 11.0f);
 
         ProbabilidadeAdocao probabilidade = actCalculateProbability(pet);
 
         assertEquals(ProbabilidadeAdocao.BAIXA, probabilidade);
     }
+
+    @Test
+    @DisplayName("Testa Probabilidade de Adoção de Cachorro velho com pouco peso")
+    void deveriaCalcularProbabilidadeAdocaoCachorroMedia() {
+
+        Pet pet = arrangePet(TipoPet.CACHORRO,16, 16.0f);
+
+        ProbabilidadeAdocao probabilidade = actCalculateProbability(pet);
+
+        assertEquals(ProbabilidadeAdocao.MEDIA, probabilidade);
+    }
+
+    private static ProbabilidadeAdocao actCalculateProbability(Pet pet) {
+        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+        ProbabilidadeAdocao probabilidade = calculadora.calcular(pet);
+        return probabilidade;
+    }
+
+    private static Pet arrangePet(TipoPet tipoPet, int idade, float peso) {
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo feliz",
+                "94999999999",
+                "abrigofeliz@email.com.br"
+        ));
+
+        Pet pet = new Pet(new CadastroPetDto(
+                tipoPet,
+                "Miau",
+                "Siames",
+                idade,
+                "Cinza",
+                peso
+        ), abrigo);
+        return pet;
+    }
+
 }
