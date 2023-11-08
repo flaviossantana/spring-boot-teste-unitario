7653a4a4c486f907a92355be7a758874df4a60c3
flavio.santana <9090flavio@gmail.com>
2023-11-08 18:45:40 -0300

    feat: Utilizando Mocks

--- src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
+++ src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -4,11 +4,14 @@ import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.repository.PetRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ValidacaoPetDisponivelTest {
 
     @InjectMocks
@@ -22,6 +25,5 @@ class ValidacaoPetDisponivelTest {
         SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(1L,1L, "");
 
         Assertions.assertDoesNotThrow(() -> validacaoPetDisponivel.validar(dto));
-
     }
 }
