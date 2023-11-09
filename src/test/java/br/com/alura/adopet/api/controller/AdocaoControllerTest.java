04e482044841b4eed1089644ec955d55f5489dbb
flavio.santana <9090flavio@gmail.com>
2023-11-09 12:11:44 -0300

    feat: Testando outros cenários

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,16 +1,20 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.service.AdocaoService;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
@@ -20,6 +24,23 @@ class AdocaoControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private AdocaoService adocaoService;
+
+    @Test
+    void deveriaSolicitacaoAdocao() throws Exception {
+
+        MockHttpServletResponse response = this.mockMvc
+                .perform(
+                        post("/adocoes")
+                                .content(new Gson().toJson(new SolicitacaoAdocaoDto(1L,1L, "Meu Motivo")))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+
+        assertEquals(OK.value(), response.getStatus());
+    }
 
     @Test
     void deveriaValidarSolicitacaoAdocao() throws Exception {
