8f358277a566f758f6aa1b629708d726d2c4a5e1
flavio.santana <9090flavio@gmail.com>
2023-11-09 11:25:24 -0300

    feat: Validando objetos com ArgumentCaptor

--- src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -1,22 +1,26 @@
 package br.com.alura.adopet.api.service;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Abrigo;
 import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.PetRepository;
 import br.com.alura.adopet.api.repository.TutorRepository;
 import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,7 +29,7 @@ class AdocaoServiceTest {
     @InjectMocks
     private AdocaoService  adocaoService;
     @Mock
-    private AdocaoRepository repository;
+    private AdocaoRepository adocaoRepository;
     @Mock
     private PetRepository petRepository;
     @Mock
@@ -35,13 +39,34 @@ class AdocaoServiceTest {
     @Mock
     private List<ValidacaoSolicitacaoAdocao> validacoes;
     @Mock
-    private SolicitacaoAdocaoDto solicitacaoAdocaoDto;
+    private Pet pet;
+    @Mock
+    private Abrigo abrigo;
+    @Mock
+    private Tutor tutor;
+    @Captor
+    private ArgumentCaptor<Adocao> adocaoCaptor;
 
     @Test
     void deveriaSolicitarAdocao() {
 
-        adocaoService.solicitar(solicitacaoAdocaoDto);
+        SolicitacaoAdocaoDto solicitacaoAdocao = new SolicitacaoAdocaoDto(1L, 1L, "motivo");
 
-        then(repository).should().save(any(Adocao.class));
+        given(petRepository.getReferenceById(solicitacaoAdocao.idPet()))
+                .willReturn(pet);
+        given(tutorRepository.getReferenceById(solicitacaoAdocao.idTutor()))
+                .willReturn(tutor);
+        given(pet.getAbrigo())
+                .willReturn(abrigo);
+
+
+        this.adocaoService.solicitar(solicitacaoAdocao);
+
+
+        then(adocaoRepository).should().save(adocaoCaptor.capture());
+        Adocao adocao = adocaoCaptor.getValue();
+        assertEquals(pet, adocao.getPet());
+        assertEquals(tutor, adocao.getTutor());
+        assertEquals(solicitacaoAdocao.motivo(), adocao.getMotivo());
     }
 }
